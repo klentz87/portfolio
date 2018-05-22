f02765c1553a6e7c9d77b24fee789bf947fb366e
Kris E. Lentz <kris.e.lentz@gmail.com>
2018-05-22 18:42:32 -0500

    finished css for mobile

--- second-draft/scripts/main.js
+++ second-draft/scripts/main.js
@@ -15,52 +15,66 @@ window.onload = function() {
       var clicked = e.target
       var sectionId = clicked.innerHTML.toLowerCase()
       var currentDisplay = '#' + ($('.current').attr('id'));
-      console.log(sectionId);
-      console.log(currentDisplay)
       if (currentDisplay != ('#' + sectionId)) {
         if (sectionId == 'about') {
+          $('#about').removeClass('hidden');
+          $('#about').addClass('display');
           $(currentDisplay).animate(
-              {left: "-200%"}, 500,
+              {left: "-220%"}, 500,
               function() {
                 $('#about').animate(
                 {left: '50%'})
               });
+            $('#about').css('left','220%');
+        /*  $(currentDisplay).addClass('hidden'); */
             $(currentDisplay).removeClass('current');
             $('#about').addClass('current');
             currentDisplay = '#' + ($('.current').attr('id'))
           };
 
         if (sectionId == 'education') {
+          $('#education').removeClass('hidden');
+          $('#education').addClass('display');
           $(currentDisplay).animate(
-              {left: "-200%"}, 500,
+              {left: "-220%"}, 500,
                 function() {
                   $('#education').animate(
                   {left: '50%'})
                 });
+            $('#education').css('left','220%');
+        /*  $(currentDisplay).addClass('hidden'); */
             $(currentDisplay).removeClass('current');
             $('#education').addClass('current');
             currentDisplay = '#' + ($('.current').attr('id'))
         };  
 
         if (sectionId == 'experience') {
+          $('#experience').removeClass('hidden');
+          $('#experience').addClass('display');      
           $(currentDisplay).animate(
-              {left: "-200%"}, 500,
+              {left: "-220%"}, 500,
                 function() {
                   $('#experience').animate(
                   {left: '50%'})
                 });
+            $('#experience').css('left','220%');     
+        /*  $(currentDisplay).addClass('hidden'); */
             $(currentDisplay).removeClass('current');
             $('#experience').addClass('current');
             currentDisplay = '#' + ($('.current').attr('id'))
         };
 
         if (sectionId == 'projects') {
+          $('#projects').removeClass('hidden');
+          $('#projects').addClass('display');
           $(currentDisplay).animate(
-              {left: "200%"}, 500,
+              {left: "-220%"}, 500,
                 function() {
                   $('#projects').animate(
                   {left: '50%'})
                 });
+            $('#projects').css('left','220%');
+        /*  $(currentDisplay).addClass('hidden'); */
             $(currentDisplay).removeClass('current');
             $('#projects').addClass('current');
             currentDisplay = '#' + ($('.current').attr('id'))
